935fdb0f426d78ef888b1dc35c79f3d2c65df2b8
Gustavo64522 <gustavo64522@gmail.com>
2025-02-20 18:01:07 -0300

    feat: Added and styled comments component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/54072507?v=4" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Andrade</strong>
+              <time
+                title="20 de Fevereiro às 16:16"
+                dateTime="2025-02-20 16:16:0"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentario">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir
+            <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
