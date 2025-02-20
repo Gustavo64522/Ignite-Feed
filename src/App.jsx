4a17b807e6c6c94466d5b6ab4e67dd1fc3d0dcb1
Gustavo64522 <gustavo64522@gmail.com>
2025-02-20 16:40:51 -0300

    feat: Add post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
 
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -12,7 +13,10 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>POST</main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
