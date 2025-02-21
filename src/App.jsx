598ddbc989cf210e32ea3bbe03c7f83aaea66c04
Gustavo64522 <gustavo64522@gmail.com>
2025-02-21 15:28:54 -0300

    feat: Adding dynamic data to the Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,50 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+// Dados dinamicos
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @ Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-02-21 15:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "CCO @ Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala pessoal 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻",
+      },
+      { type: "link", content: "👉 devonlane.design" },
+    ],
+    publishedAt: new Date("2025-02-21 15:15:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,8 +58,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
